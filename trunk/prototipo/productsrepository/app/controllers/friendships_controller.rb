4cd3b39972882e51243b8a79f18894210a65a65d
leobessa <leobessa@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-08-30 23:12:55 +0000

    rspec para controlador de friendships
    git-svn-id: http://subversion.assembla.com/svn/olimpo@86 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/prototipo/productsrepository/app/controllers/friendships_controller.rb
+++ trunk/prototipo/productsrepository/app/controllers/friendships_controller.rb
@@ -1,4 +1,5 @@
-class FriendshipsController < ApplicationController
+class FriendshipsController < ApplicationController  
+  
   def create
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
--- trunk/prototipo/productsrepository/spec/controllers/friendships_controller_spec.rb
+++ trunk/prototipo/productsrepository/spec/controllers/friendships_controller_spec.rb
@@ -4,28 +4,41 @@ describe FriendshipsController do
   #fixtures :all
   #integrate_views
   before :each do
-    @friendship = mock_model Friendship
+    #@friendship = mock_model Friendship                     
+    #@friendships = mock_model Array
+    #@friendships.stub(:build).with(:friend_id => params[:friend_id]).and_return(@friendship)
+    #@current_user.stub(:friendships).and_return(@friendships) 
+    #@user_session = mock_model(UserSession)
+    #UserSession.stub!(:find).and_return(@user_session)
+    #@user_session.stub!(:user).and_return(@current_user)
+    #@current_user.stub(:id).and_return(1)  
+    @user_session = mock_model(UserSession)     
+    @friendships = mock_model Array
+    @user = mock_model(User,{:friendships => @friendships, :id => '12'})
+    UserSession.stub!(:find).and_return(@user_session)
+    @user_session.stub!(:user).and_return(@user)
   end
   
-  it "create action should render new template when model is invalid" do
-    Friendship.should_receive(:new).with(params[:friendship]).once.and_return(@friendship) 
+  it "create action should render new template when model is invalid" do  
+    @friendships.stub!(:build).with(:friend_id => params[:friend_id]).and_return(@friendship)
     @friendship.stub!(:save).and_return(false)
     
     post :create
-    response.should render_template(:new)
+    response.should redirect_to(root_url)
   end
   
   it "create action should redirect when model is valid" do
-    Friendship.should_receive(:new).with(params[:friendship]).once.and_return(@friendship) 
+    @friendships.stub!(:build).with(:friend_id => params[:friend_id]).and_return(@friendship)
     @friendship.stub!(:save).and_return(true) 
     post :create
     response.should redirect_to(root_url)
   end
   
   it "destroy action should destroy model and redirect to index action" do
-    friendship = Factory :friendship
-    delete :destroy, :id => friendship
-    response.should redirect_to(root_url)
-    Friendship.exists?(friendship.id).should be_false
-  end
+    @friendships.stub(:find).with('12').and_return(@friendship)
+    @friendship.stub(:destroy)
+    delete :destroy, :id => '12' 
+    response.should redirect_to(user_url('12'))
+  end   
+  
 end
