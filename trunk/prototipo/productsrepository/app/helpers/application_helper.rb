1195c873a4dfcba4271a9092caaeb79aecce5f27
leobessa <leobessa@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-09-17 05:54:10 +0000

    View para visualiazacao de rating
    git-svn-id: http://subversion.assembla.com/svn/olimpo@97 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/prototipo/productsrepository/app/helpers/application_helper.rb
+++ trunk/prototipo/productsrepository/app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
-module ApplicationHelper
+module ApplicationHelper        
+  def rating_tag(value) 
+    return unless value                                  
+    options = {:class => "current-rating", :style => "width:#{30*value}px;"}
+    content_tag(:li, "Currently #{value}/5 Stars", options)
+  end
 end
