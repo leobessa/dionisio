ffff0ee665eec5b0f1d6c8d009c0604c61e43afb
leobessa <leobessa@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-09-17 21:25:07 +0000

    Tela de recomendacao
    git-svn-id: http://subversion.assembla.com/svn/olimpo@104 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/prototipo/productsrepository/app/controllers/user_recommendations_controller.rb
+++ trunk/prototipo/productsrepository/app/controllers/user_recommendations_controller.rb
@@ -8,7 +8,7 @@ class UserRecommendationsController < ApplicationController
       if @user_recommendation.save
         flash[:notice] = 'Recommendation was successfully created.'
         format.html { redirect_to(@user_recommendation) }
-        format.xml  { render :xml => @user_recommendation, :status => :created, :location => @product }
+        format.xml  { render :xml => @user_recommendation, :status => :created, :location => @user_recommendation }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @user_recommendation.errors, :status => :unprocessable_entity }
@@ -18,7 +18,8 @@ class UserRecommendationsController < ApplicationController
   
   def new
     @user_recommendation = UserRecommendation.new
-    @user_recommendation.recommender = current_user 
+    @user_recommendation.recommender = current_user  
+    @product = Product.find(params[:product_id])
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @user_recommendation }
