dc6745c2244e89d79d81a8aa14177cf487d1f586
leobessa <leobessa@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-09-22 22:36:07 +0000

    Envio e recebimento de recomendação de usuários
    git-svn-id: http://subversion.assembla.com/svn/olimpo@109 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/prototipo/productsrepository/app/controllers/user_recommendations_controller.rb
+++ trunk/prototipo/productsrepository/app/controllers/user_recommendations_controller.rb
@@ -1,19 +1,20 @@
 class UserRecommendationsController < ApplicationController 
   
-  def create
-    @user_recommendation = UserRecommendation.new params[:user_recommendation]
-    @user_recommendation.recommender = current_user
-    
-    respond_to do |format|
-      if @user_recommendation.save
-        flash[:notice] = 'Recommendation was successfully created.'
-        format.html { redirect_to(@user_recommendation) }
-        format.xml  { render :xml => @user_recommendation, :status => :created, :location => @user_recommendation }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user_recommendation.errors, :status => :unprocessable_entity }
-      end
-    end    
+  def create    
+    target_users = params[:targert_users]  
+    if (target_users.nil? || target_users.empty?)
+      flash[:error] = "Recommendations must have a target user"  
+      redirect_to new_product_user_recommendation_path(params[:user_recommendation][:product_id])
+    else
+       target_users.each do |target|  
+          @user_recommendation = UserRecommendation.new params[:user_recommendation]
+          @user_recommendation.recommender = current_user
+          @user_recommendation.target_user_id = target 
+          @user_recommendation.save
+        end
+        flash[:notice] = "Recommendation was successfully send."
+        redirect_to @user_recommendation.product
+    end 
   end                                                                   
   
   def new
--- trunk/prototipo/productsrepository/app/models/product.rb
+++ trunk/prototipo/productsrepository/app/models/product.rb
@@ -3,14 +3,15 @@ class Product < ActiveRecord::Base
   has_many :offers
   belongs_to :category
   validates_uniqueness_of :name
-  has_many :users, :through => :reviews
+  has_many :users, :through => :reviews 
+  has_many :user_recommendations, :class_name => "UserRecommendation", :foreign_key => "product_id"
 
   def to_s
     name
   end
   
   def photo_alt
-    name.gsub(/ /, '+')
+    name.parameterize
   end
   
 end
