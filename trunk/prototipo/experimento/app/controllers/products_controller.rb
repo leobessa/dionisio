026a618bdc50eaf0ef9faea1e8e587a943d6de3d
leobessa <leobessa@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-10-27 21:56:50 +0000

    removendo log desnecessario
    git-svn-id: http://subversion.assembla.com/svn/olimpo@143 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/prototipo/experimento/app/controllers/products_controller.rb
+++ trunk/prototipo/experimento/app/controllers/products_controller.rb
@@ -8,10 +8,7 @@ class ProductsController < ApplicationController
       page.replace_html id, ratings_for(@product, :wrap => false, :dimension => params[:dimension])
       page.visual_effect :highlight, id 
       page.replace_html 'phase-description', phase_description_content                       
-      puts "rate count = #{Rate.count(:conditions => {:user_id => current_user, :rateable_id => Product.selected.map(&:id)})}"
-      puts "selected count = #{Product.selected.count}"
       if current_user.completed_stage?
-        puts "---------------------------------"
         current_user.advance_stage
         page.redirect_to(root_path)
       end
