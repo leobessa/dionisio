7af21fa4a6c881314600b5c1f6f3dd0ffa80dda2
leobessa <leobessa@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-10-31 22:14:41 +0000

    Recomendação por similaridade de perfil implementada e testada.
    git-svn-id: http://subversion.assembla.com/svn/olimpo@158 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/prototipo/experimento/app/models/recommender.rb
+++ trunk/prototipo/experimento/app/models/recommender.rb
@@ -1,30 +1,82 @@
-class Recommender                 
-  
-  def self.pearson_correlation(x,y)
-    intersection = x.rated_products & y.rated_products
-    n = intersection.length
-    return 0.0 if n == 0
-    
-    sum_x = sum_y = sum_x_sq = sum_y_sq = p_sum = 0.0                  
-    
-    ratings = Rating.find(:all, :conditions => {:user_id => [x,y], :product_id => intersection})
-    product_ratings = ratings.inject({}) do |result,rating| 
-       result[rating.product_id] ||= {}  
-       result[rating.product_id][rating.user_id] = rating.stars.to_f                         
-       result
+class Recommender 
+  class << self                
+
+    def pearson_correlation(x,y)
+      intersection = x.rated_products & y.rated_products
+      n = intersection.length
+      return 0.0 if n == 0
+
+      sum_x = sum_y = sum_x_sq = sum_y_sq = p_sum = 0.0                  
+
+      ratings = Rating.find(:all, :conditions => {:user_id => [x,y], :product_id => intersection})
+      product_ratings = ratings.inject({}) do |result,rating| 
+        result[rating.product_id] ||= {}  
+        result[rating.product_id][rating.user_id] = rating.stars.to_f                         
+        result
+      end
+      product_ratings.values.each do |rate|
+        sum_x += rate[x.id]
+        sum_y += rate[y.id]
+        sum_x_sq += (rate[x.id])**2
+        sum_y_sq += (rate[y.id])**2
+        p_sum += rate[x.id] * rate[y.id] 
+      end
+
+      num = p_sum - ((sum_x*sum_y)/n)
+      den = Math.sqrt((sum_x_sq - (sum_x ** 2)/n)*(sum_y_sq - (sum_y ** 2)/n))
+      return 0.0 if den == 0.0
+      num/den
     end
-    product_ratings.values.each do |rate|
-      sum_x += rate[x.id]
-      sum_y += rate[y.id]
-      sum_x_sq += (rate[x.id])**2
-      sum_y_sq += (rate[y.id])**2
-      p_sum += rate[x.id] * rate[y.id] 
+    alias sim_pearson pearson_correlation
+
+  end                                    
+
+  module ProfileBased 
+
+    def self.similar_users(user,options = {})
+      defaults = { :min_similarity => 0.0}
+      options = defaults.merge(options)
+      similar_users = []
+      User.all.each do |other|
+        next if other == user
+        similarity = Recommender.pearson_correlation(user,other)
+        next if similarity <= options[:min_similarity]
+        similar_users << other
+        yield user,similarity if block_given?
+      end                     
+      similar_users
     end
-    
-    num = p_sum - ((sum_x*sum_y)/n)
-    den = Math.sqrt((sum_x_sq - (sum_x ** 2)/n)*(sum_y_sq - (sum_y ** 2)/n))
-    return 0 if den == 0
-    num/den
-  end 
-  
+
+    def self.recommendations_for(user,options = {})
+      defaults = {:limit => 5}
+      options = defaults.merge options
+
+      totals  = {}
+      totals.default= 0.0
+      similarity_sum = {}
+      similarity_sum.default= 0.0
+
+      self.similar_users(user) do |other,similarity|                         
+        # Isso me parace um workarround de um bug do rails!                        
+        ids = user.rated_product_ids.empty? ? false  : user.rated_product_ids
+        ratings = Rating.find :all, :conditions => ['user_id = ? and product_id NOT IN (?)',other,ids]
+        ratings.each do |rating|
+          totals[rating.product_id] += (rating.stars.to_f * similarity)
+          similarity_sum[rating.product_id] += similarity
+        end
+      end
+                   
+
+      rankings = []
+      totals.each_pair do |product_id,total|
+        item = Product.find(product_id)
+        def item.recommendation_score; @recommendation_score ; end;
+        def item.recommendation_score=(d);  @recommendation_score = d ; end;
+        item.recommendation_score =  (total/similarity_sum[product_id])
+        rankings << item
+      end
+      rankings.sort{|x,y| y.recommendation_score <=> x.recommendation_score } [0,options[:limit]]
+    end
+
+  end
 end
--- trunk/prototipo/experimento/spec/models/recommender_spec.rb
+++ trunk/prototipo/experimento/spec/models/recommender_spec.rb
@@ -8,7 +8,7 @@ describe Recommender do
     it "should be 0 when intersection is empty" do
       @foo = Factory.create :user
       @boo = Factory.create :user
-      Recommender.pearson_correlation(@foo,@boo).should == 0
+      Recommender.pearson_correlation(@foo,@boo).should == 0.0
     end
 
 
@@ -18,13 +18,13 @@ describe Recommender do
       @product = Factory :product
       Factory :rating, :user => @foo, :product => @product, :stars => 3
       Factory :rating, :user => @boo, :product => @product, :stars => 4
-      Recommender.pearson_correlation(@foo,@boo).should be 0
+      Recommender.pearson_correlation(@foo,@boo).should be_close(0.0,0.001)
     end
-    
+
     it "should be the pearson correlaction when intersection has 2 items" do
       x = Factory.create :user
       y = Factory.create :user 
-      
+
       [{x => 1, y => 2},{x => 2, y => 5}].each do |point|               
         product = Factory :product
         point.each_pair do |user,stars|
@@ -34,11 +34,11 @@ describe Recommender do
 
       Recommender.pearson_correlation(x,y).should == 1.0
     end
-    
+
     it "should be the pearson correlaction when intersection has more than 2 items" do
       x = Factory.create :user
       y = Factory.create :user 
-      
+
       [{x => 2, y => 5},{x => 3, y => 1},{x => 4, y => 2}].each do |point|               
         product = Factory :product
         point.each_pair do |user,stars|
@@ -48,38 +48,81 @@ describe Recommender do
 
       Recommender.pearson_correlation(x,y).should be_close(-0.721,0.001)
     end
-    
-     it "should be -1 when intersection user ratings are totally different" do
-        x = Factory.create :user
-        y = Factory.create :user 
 
-        [{x => 1, y => 5},{x => 5, y => 1},{x => 4, y => 2}].each do |point|               
-          product = Factory :product
-          point.each_pair do |user,stars|
-            Factory :rating, :user => user, :product => product  , :stars => stars
-          end
+    it "should be -1 when intersection user ratings are totally different" do
+      x = Factory.create :user
+      y = Factory.create :user 
+
+      [{x => 1, y => 5},{x => 5, y => 1},{x => 4, y => 2}].each do |point|               
+        product = Factory :product
+        point.each_pair do |user,stars|
+          Factory :rating, :user => user, :product => product  , :stars => stars
         end
-
-        Recommender.pearson_correlation(x,y).should be_close(-1.0,0.001)
       end
-      
-      it "should be 1 when intersection user ratings are totally equal" do
-          x = Factory.create :user
-          y = Factory.create :user 
 
-          [{x => 5, y => 5},{x => 1, y => 1},{x => 2, y => 2}].each do |point|               
-            product = Factory :product
-            point.each_pair do |user,stars|
-              Factory :rating, :user => user, :product => product  , :stars => stars
-            end
-          end
+      Recommender.pearson_correlation(x,y).should be_close(-1.0,0.001)
+    end
 
-          Recommender.pearson_correlation(x,y).should == 1.0
+    it "should be 1 when intersection user ratings are totally equal" do
+      x = Factory.create :user
+      y = Factory.create :user 
+
+      [{x => 5, y => 5},{x => 1, y => 1},{x => 2, y => 2}].each do |point|               
+        product = Factory :product
+        point.each_pair do |user,stars|
+          Factory :rating, :user => user, :product => product  , :stars => stars
         end
+      end
+
+      Recommender.pearson_correlation(x,y).should == 1.0
+    end
+    
+    it "should find users with similar profiles" do
+      user = Factory :user
+      similar_user = Factory :user
+      not_similar_user = Factory :user
+      Recommender.should_receive(:pearson_correlation).with(user,similar_user).and_return(1.0)
+      Recommender.should_receive(:pearson_correlation).with(user,not_similar_user).and_return(-1.0)
+      similar_users = Recommender::ProfileBased.similar_users(user)
+      similar_users.should include(similar_user)
+      similar_users.should_not include(not_similar_user)
+      similar_users.should_not include(user)
+    end
 
   end
+  
 
-  it "should make profile based recommendations"
+  context "making profile based recommendations" do 
+
+    it "should be empty when user has no ratings" do
+      x = Factory.create :user
+      Recommender::ProfileBased.recommendations_for(x).should be_empty   
+    end 
+    
+    it "should find unrated products which are nice to similiar users" do                                                            
+      user_x = Factory :user
+      similiar_friend = Factory :user
+      product = Factory :product    
+      Factory :rating, :user => user_x,         :stars => 4
+      Factory :rating, :user => similiar_friend, :stars => 4, :product => product
+      Recommender::ProfileBased.should_receive(:similar_users).with(user_x).and_yield(similiar_friend,0.5)
+      recommendations = Recommender::ProfileBased.recommendations_for(user_x)
+      recommendations.should include(product) 
+      recommendations.length.should == 1
+      recommendations.first.recommendation_score.should == 4
+    end
+    
+    it "should not recommend products that user has already rated" do                                                            
+      user_x = Factory :user
+      similiar_friend = Factory :user
+      product = Factory :product
+      Factory :rating, :user => user_x,         :stars => 4, :product => product
+      Factory :rating, :user => similiar_friend, :stars => 4, :product => product
+      Recommender::ProfileBased.should_receive(:similar_users).with(user_x).and_yield(similiar_friend,0.5)
+      Recommender::ProfileBased.recommendations_for(user_x).should_not include(product)   
+    end
+    
+  end
 
   it "should make item based recommendations"
 
