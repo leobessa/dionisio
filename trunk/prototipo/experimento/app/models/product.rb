7c3111133695e0e37ea9e82bb4deead55fc83a2e
douglaz <douglaz@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-11-05 00:21:04 +0000

    checkbox voltou, mas sem ajudar muito
    git-svn-id: http://subversion.assembla.com/svn/olimpo@203 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/prototipo/experimento/app/models/product.rb
+++ trunk/prototipo/experimento/app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  default_scope :order => 'popularity DESC'
+  default_scope :order => 'popularity DESC, name DESC'
   belongs_to :category
   has_many :ratings
   named_scope :selected, :conditions => {:selected => true}
