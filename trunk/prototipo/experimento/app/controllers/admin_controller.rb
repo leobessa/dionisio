f7f8cc1dbc303f76927956c6164fe3a6db59e9ad
Leonardo Bessa <leobessa@gmail.com>
2009-11-18 19:29:18 -0200

    ordenando usuário por atualizacao

--- trunk/prototipo/experimento/app/controllers/admin_controller.rb
+++ trunk/prototipo/experimento/app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
   before_filter :authenticate_admin!                           
   
   def index
-    @users = User.all
+    @users = User.find(:all, :order => :updated_at)
   end
 
 end
