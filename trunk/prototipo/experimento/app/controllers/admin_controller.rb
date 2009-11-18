e8277f0a6dfe6c7ab5ed1c894b880fad2ba8e955
leobessa <leobessa@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-11-18 21:38:48 +0000

    ordenando usuário corretamente
    git-svn-id: http://subversion.assembla.com/svn/olimpo@225 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/prototipo/experimento/app/controllers/admin_controller.rb
+++ trunk/prototipo/experimento/app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
   before_filter :authenticate_admin!                           
   
   def index
-    @users = User.find(:all, :order => :updated_at)
+    @users = User.find(:all, :order => 'updated_at DESC')
   end
 
 end
