9bbbf201b55f26d0313180de6a5b6af79a4e9210
douglaz <douglaz@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-12-02 00:49:38 +0000

    Graficos da taxa de serendipidade serendipidade
    git-svn-id: http://subversion.assembla.com/svn/olimpo@286 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/monografia/gen_graphs_tables.py
+++ trunk/monografia/gen_graphs_tables.py
@@ -59,7 +59,7 @@ def gen_bar(x_labels, graph, out_name, yformat='%g', title='', xlabel='', ylabel
 
 def gen_erro(c):
     graph = {}
-    c.execute("select avg(abs(r.stars-sr.predicted_rating))*100/4 as erro, stddev(abs(r.stars-sr.predicted_rating))*100/4, sr.algorithm from ratings r, system_recommendations sr where r.product_id = sr.product_id and r.user_id = sr.user_id group by sr.algorithm union all select avg(abs(5-r.stars))*100/4, stddev(abs(5-r.stars))*100/4, 'direto' from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id")
+    c.execute("select avg(abs(r.stars-sr.predicted_rating))*100/4 as erro, stddev(abs(r.stars-sr.predicted_rating))*100/4, sr.algorithm from ratings r, system_recommendations sr where r.product_id = sr.product_id and r.user_id = sr.user_id group by sr.algorithm union all select avg(abs(5-r.stars))*100/4, stddev(abs(5-r.stars))*100/4, 'direta' from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id")
     for erro_medio, erro_desvio, algoritmo in c.fetchall():
         graph[algoritmo] = [erro_medio, erro_desvio]
     print graph
@@ -75,7 +75,7 @@ def gen_notas(c):
         agrupa = {1:0, 2:0, 3: 1, 4: 2, 5: 2}
         graph.setdefault(algoritmo, [0,0,0])
         graph[algoritmo][agrupa[faixa]] += p
-    print graph
+        
     gen_bar(['1-2', '3', '4-5'], graph, 'grafico_notas.pdf',
         title='Porcentagem das notas em cada faixa comparadas por tipo de recomendação',
         yformat='%g%%', sort=True, sort_col=0)
@@ -102,6 +102,23 @@ def gen_notas_medias(c):
     gen_bar(['Média', 'Desvio-Padrão'], graph, 'grafico_notas_medias_diretas.pdf',
         title='Notas das recomendações diretas',yformat='%g', sort=True, sort_col=0)
     
+def gen_serendipidade(c):
+    graph = {}
+    c.execute("select count(*)*100/(select count(*) from system_recommendations _sr, users _u where _sr.algorithm = sr.algorithm and _u.id = _sr.user_id and _u.stage_number >=6), sr.algorithm from ratings r, system_recommendations sr, users u where r.product_id = sr.product_id and r.user_id = sr.user_id and u.id = r.user_id and u.stage_number >=6 and r.stars >=4 and r.unknown='t' group by sr.algorithm union all select count(*)*100/(select count(*) from user_recommendations _ur, users _u where _u.id = _ur.target_id and _u.stage_number >=6), 'direta' from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id and u.stage_number >=6 and r.stars >= 4 and r.unknown='t'")
+    for taxa, algoritmo in c.fetchall():
+        graph[algoritmo] = [taxa]
+    gen_bar(['Taxa de Serendipidade'], graph, 'grafico_serendipidade.pdf',
+        title='Taxa de serendipidade por tipo de recomendação',
+        yformat='%g%%', sort=True, sort_col=0)
+    
+    graph = {}
+    c.execute("select count(*)*100/(select count(*) from user_recommendations _ur, users _u where _u.id = _ur.target_id and _u.stage_number >=6), 'amigos' from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id and u.stage_number >=6 and r.stars >= 4 and r.unknown='t' and ur.sender_id in (select _u.id from users _u where _u.group_id = u.group_id and _u.id <> u.id) union all select count(*)*100/(select count(*) from user_recommendations _ur, users _u where _u.id = _ur.target_id and _u.stage_number >=6), 'desconhecidos' from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id and u.stage_number >=6 and r.stars >= 4 and r.unknown='t' and ur.sender_id in (select _u.id from users _u where _u.group_id <> u.group_id and _u.id <> u.id)")
+    for taxa, algoritmo in c.fetchall():
+        graph[algoritmo] = [taxa]
+    gen_bar(['Taxa de Serendipidade'], graph, 'grafico_serendipidade_diretas.pdf',
+        title='Taxa de serendipidade comparando amigos e desconhecidos',
+        yformat='%g%%', sort=True, sort_col=0)
+    
 def main():
     con = sqlite3.connect('../prototipo/experimento/db/production.sqlite3')
     con.create_aggregate("stddev", 1, StdDev)
@@ -109,5 +126,6 @@ def main():
     gen_erro(c)
     gen_notas(c)
     gen_notas_medias(c)
+    gen_serendipidade(c)
 
 main()
