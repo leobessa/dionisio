65f0853281104d0c0cda5c28b2f9ba2bcffd36cb
douglaz <douglaz@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-12-02 00:11:19 +0000

    Graficos e mudancas nos scripts
    git-svn-id: http://subversion.assembla.com/svn/olimpo@282 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/monografia/gen_graphs_tables.py
+++ trunk/monografia/gen_graphs_tables.py
@@ -75,17 +75,47 @@ def gen_notas(c):
     print graph
     gen_bar(['1-2', '3', '4-5'], graph, 'grafico_notas.pdf',
         yformat='%g%%', sort=True, sort_col=0)
-    graph = inverte(graph)
+
+def gen_notas(c):
+    graph = {}
+    c.execute("select r.stars as nota, count(*)*100/(select count(*) from system_recommendations _sr, users _u where _sr.algorithm = sr.algorithm and _u.id = _sr.user_id and _u.stage_number >=6), sr.algorithm from ratings r, system_recommendations sr, users u where r.product_id = sr.product_id and r.user_id = sr.user_id and u.id = sr.user_id and u.stage_number >=6 group by nota, sr.algorithm union all select r.stars as nota, count(*)*100/(select count(*) from user_recommendations _ur, users _u where _u.id = _ur.target_id and _u.stage_number >=6), 'direta' from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id and u.stage_number >=6 group by nota")
+    for faixa, p, algoritmo in c.fetchall():
+        agrupa = {1:0, 2:0, 3: 1, 4: 2, 5: 2}
+        graph.setdefault(algoritmo, [0,0,0])
+        graph[algoritmo][agrupa[faixa]] += p
     print graph
-    gen_bar(['a','b','c','d'], graph, 'grafico_notas_inv.pdf',
-        yformat='%g%%')
+    gen_bar(['1-2', '3', '4-5'], graph, 'grafico_notas.pdf',
+        title='Porcentagem das notas em cada faixa comparadas por tipo de recomendação',
+        yformat='%g%%', sort=True, sort_col=0)
 
-
+def gen_notas_medias(c):
+    graph = {}
+    c.execute("select avg(r.stars), stddev(r.stars) from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id")
+    graph['diretas'] = c.fetchone()
+    
+    c.execute("select avg(r.stars), stddev(r.stars), sr.algorithm from ratings r, system_recommendations sr where r.product_id = sr.product_id and r.user_id = sr.user_id group by sr.algorithm")
+    
+    for media, desvio, algoritmo in c.fetchall():
+        graph[algoritmo] = (media, desvio)
+    gen_bar(['Média', 'Desvio-Padrão'], graph, 'grafico_notas_medias.pdf',
+        title='Notas por tipo de recomendação',yformat='%g', sort=True, sort_col=0)
+    
+    graph = {}
+    c.execute("select avg(r.stars), stddev(r.stars) from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id and ur.sender_id in (select _u.id from users _u where _u.group_id = u.group_id and _u.id <> u.id)")
+    graph['amigos'] = c.fetchone()
+    
+    c.execute("select avg(r.stars), stddev(r.stars) from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id and ur.sender_id in (select _u.id from users _u where _u.group_id <> u.group_id and _u.id <> u.id)")
+    graph['desconhecidos'] = c.fetchone()
+    
+    gen_bar(['Média', 'Desvio-Padrão'], graph, 'grafico_notas_medias_diretas.pdf',
+        title='Notas das recomendações diretas',yformat='%g', sort=True, sort_col=0)
+    
 def main():
     con = sqlite3.connect('../prototipo/experimento/db/production.sqlite3')
     con.create_aggregate("stddev", 1, StdDev)
     c = con.cursor()
     gen_erro(c)
     gen_notas(c)
+    gen_notas_medias(c)
 
 main()
--- trunk/monografia/test_normal.py
+++ trunk/monografia/test_normal.py
@@ -3,7 +3,8 @@
 
 
 import sqlite3
-from scipy.stats import shapiro, ttest_ind
+from scipy.stats import *
+from numpy import *
 
 def print_per_line(f, l, n=10):
     for i, c in enumerate(l):
@@ -13,12 +14,20 @@ def print_per_line(f, l, n=10):
         else:
             f.write('\t')
 
+def tests(s1, s2):
+    print 'student:', ttest_ind(s1, s2)[1]
+    print 'mann whitney:', mannwhitneyu(s1, s2)[1]*2
+    print 'wilcoxon ranksum:', ranksums(s1, s2)[1]
+    n = min(len(s1), len(s2))
+    print 'wilcoxon:', wilcoxon(s1[:n], s2[:n])[1]
+    print 'kruskal:', kruskal(array(s1), array(s2))[1]
+
 def main():
     con = sqlite3.connect('../prototipo/experimento/db/production.sqlite3')
     c = con.cursor()
     c.execute('select r.stars from ratings r, users u where u.id = r.user_id and u.stage_number >=6')
     #print [ v[0] for v in c.fetchall()]
-    print shapiro([ v[0] for v in c.fetchall()])
+    print normaltest([ v[0] for v in c.fetchall()])
 
     c.execute('select r.stars from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id and ur.sender_id in (select distinct _u.id from users _u where _u.group_id = u.group_id and _u.id <> u.id)')
     amigos = [ v[0] for v in c.fetchall() ]
@@ -37,12 +46,23 @@ def main():
 
     graph = {}
     for rating, algoritmo in c.fetchall():
-        print rating,algoritmo
         graph.setdefault(algoritmo, [])
         graph[algoritmo].append(rating)
-    print graph
     for algoritmo, values in graph.items():
         print_per_line(file('%s.txt' % algoritmo, 'w+'), values)
-    print ttest_ind(amigos, nao_amigos)
+    print '\nAmigos x desconhecidos:'
+    tests(amigos, nao_amigos)
+    
+    print '\nConfianca x direta:'
+    tests(graph['trust'], diretas)
+    
+    print '\nConfianca x desconhecidos:'
+    tests(graph['trust'], nao_amigos)
+    
+    print '\nConfianca x profile:'
+    tests(graph['trust'], graph['profile'])
+    
+    print '\nConfianca x item:'
+    tests(graph['trust'], graph['item'])
     
 main()
