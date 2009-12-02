0c7d68b376c4d95d9801709825ea854911184b55
douglaz <douglaz@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-12-02 03:15:54 +0000

    Consertado dedicatoria
    git-svn-id: http://subversion.assembla.com/svn/olimpo@296 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/monografia/test_normal.py
+++ trunk/monografia/test_normal.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*- coding: ISO-8859-1 -*-
+#-*- coding: utf-8 -*-
 
 
 import sqlite3
@@ -14,20 +14,42 @@ def print_per_line(f, l, n=10):
         else:
             f.write('\t')
 
-def tests(s1, s2):
-    print 'student:', ttest_ind(s1, s2)[1]
-    print 'mann whitney:', mannwhitneyu(s1, s2)[1]*2
-    print 'wilcoxon ranksum:', ranksums(s1, s2)[1]
-    n = min(len(s1), len(s2))
-    print 'wilcoxon:', wilcoxon(s1[:n], s2[:n])[1]
-    print 'kruskal:', kruskal(array(s1), array(s2))[1]
+def tests(title, s1, s2):
+    tipos_testes = [
+        ('T-Student (t)', ttest_ind(s1, s2)),
+        ('Mann-Whitney rank test (u)', (mannwhitneyu(s1, s2)[0], mannwhitneyu(s1, s2)[1]*2)),
+        ('Wilcoxon rank-sum test (z)', ranksums(s1, s2)),
+        ('Kruskal-Wallis H-test (H)', kruskal(array(s1), array(s2)))
+    ]
+    
+    out = r"""
+\begin{table}
+\centering
+\begin{tabular}{|r|c|c|}
+    \hline
+    \textbf{Tipo do teste} & \textbf{Valor da estatística} & \textbf{\textit{p}} \\
+    \hline
+"""
+    for teste, (est, p) in tipos_testes:
+        out += r"%s & %s & %s \\" % (teste, est, p)
+        out += "\n"
+        out += r"\hline "
+        out += "\n"
+    out += """
+    \end{tabular}
+\caption{\it %s}
+\end{table}
+""" % title
+    print out
+    #n = min(len(s1), len(s2))
+    #print 'wilcoxon:', wilcoxon(s1[:n], s2[:n])[1]
 
 def main():
     con = sqlite3.connect('../prototipo/experimento/db/production.sqlite3')
     c = con.cursor()
-    c.execute('select r.stars from ratings r, users u where u.id = r.user_id and u.stage_number >=6')
+    #c.execute('select r.stars from ratings r, users u where u.id = r.user_id and u.stage_number >=6')
     #print [ v[0] for v in c.fetchall()]
-    print normaltest([ v[0] for v in c.fetchall()])
+    #print normaltest([ v[0] for v in c.fetchall()])
 
     c.execute('select r.stars from ratings r, user_recommendations ur, users u where r.product_id = ur.product_id and r.user_id = ur.target_id and u.id = r.user_id and ur.sender_id in (select distinct _u.id from users _u where _u.group_id = u.group_id and _u.id <> u.id)')
     amigos = [ v[0] for v in c.fetchall() ]
@@ -50,19 +72,16 @@ def main():
         graph[algoritmo].append(rating)
     for algoritmo, values in graph.items():
         print_per_line(file('%s.txt' % algoritmo, 'w+'), values)
-    print '\nAmigos x desconhecidos:'
-    tests(amigos, nao_amigos)
+    tests('Teste de que Amigos recomendam melhor do que Desconhecidos (H1)',
+            amigos, nao_amigos)
     
-    print '\nConfianca x direta:'
-    tests(graph['trust'], diretas)
+    tests('Teste de que recomendações Diretas são melhores que RBC (H1)',
+        graph['trust'], diretas)
     
-    print '\nConfianca x desconhecidos:'
-    tests(graph['trust'], nao_amigos)
+    tests('Teste de que recomendações RBC é melhor que RBP (H1)',
+        graph['trust'], graph['profile'])
     
-    print '\nConfianca x profile:'
-    tests(graph['trust'], graph['profile'])
-    
-    print '\nConfianca x item:'
-    tests(graph['trust'], graph['item'])
+    tests('Teste de que recomendações RBI é melhor que RBC (H1)',
+        graph['trust'], graph['item'])
     
 main()
