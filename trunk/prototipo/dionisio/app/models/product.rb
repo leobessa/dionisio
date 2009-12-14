15c031037b66b6b49ed21defd8d51ed6beff4dd3
Leonardo Bessa <leobessa@gmail.com>
2009-12-13 22:49:24 -0200

    selecionando produtos

--- trunk/prototipo/dionisio/app/models/product.rb
+++ trunk/prototipo/dionisio/app/models/product.rb
@@ -4,7 +4,11 @@ class Product < ActiveRecord::Base
 
   belongs_to :category
   has_many :ratings
-  named_scope :most_dispersed, :conditions => {:selected => true}
+  named_scope :most_dispersed, :joins => :ratings,
+  :select => "'products'.* ,(('ratings'.stars-avg('ratings'.'stars'))*('ratings'.'stars'-avg('ratings'.'stars'))/count(*)) as 'variance'", 
+  :group => :product_id, :order => 'variance DESC', :limit => 10
+  named_scope :selected, :conditions => {:selected => true}
+  
   named_scope :with_ratings_from, (lambda do |user|
     {:select =>  "'products'.*, 'ratings'.stars as 'rating'", 
       :joins => "LEFT JOIN 'ratings' ON 'ratings'.'product_id' = 'products'.id AND 'ratings'.'user_id' = #{user.id}",
